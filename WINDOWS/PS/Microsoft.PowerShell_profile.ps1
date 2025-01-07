de978fc1cda831868be2a7e1d2f44e9122c38b92
nrnr <callmev@outlook.com>
2025-01-07 21:08:20 +0800

    Update Microsoft.PowerShell_profile.ps1

--- WINDOWS/PS/Microsoft.PowerShell_profile.ps1
+++ WINDOWS/PS/Microsoft.PowerShell_profile.ps1
@@ -6,6 +6,7 @@ oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json"  |  
  
 # Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # 设置 Tab 键，命令行自动补全和提示
+Invoke-Expression (& { (lua $HOME/z.lua/z.lua --init powershell once enhanced) -join "`n" })
 # Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
 # Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
 # Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
