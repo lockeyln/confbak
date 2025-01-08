1de902d146262c9893c3e34586a2134b6b570724
nrnr <callmev@outlook.com>
2025-01-08 17:16:34 +0800

    Update Microsoft.PowerShell_profile.ps1

--- WINDOWS/PS/Microsoft.PowerShell_profile.ps1
+++ WINDOWS/PS/Microsoft.PowerShell_profile.ps1
@@ -1,20 +1,21 @@
-Import-Module posh-git # 引入 posh-git
-Import-Module oh-my-posh # 引入 oh-my-posh
-Import-Module -Name Terminal-Icons  # 引入文件图标库
+Import-Module posh-git # git
+Import-Module -Name Terminal-Icons  # 文件图标库
+Import-Module PSReadLine  # 提供语法高亮、错误提示、多行编辑、键绑定、历史记录搜索等功能
 
-Import-Module PSReadLine
-Set-PSReadLineOption -EditMode Emacs
-Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadLineOption -BellStyle None
-Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+Invoke-Expression (& { (lua $HOME/z.lua/z.lua --init powershell once enhanced) -join "`n" })
 
 # 设置主题
 oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json"  |  Invoke-Expression
+
  
+# Set-PSReadLineOption -EditMode Emacs
+# Set-PSReadLineOption -PredictionSource HistoryAndPlugin
+# Set-PSReadLineOption -PredictionViewStyle ListView
+# Set-PSReadLineOption -BellStyle None
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar 
+
 # Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
 Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # 设置 Tab 键，命令行自动补全和提示
-Invoke-Expression (& { (lua $HOME/z.lua/z.lua --init powershell once enhanced) -join "`n" })
 # Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
 # Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
 # Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
