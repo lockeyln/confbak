3f3019e62c158ea42ee52ff5d6f4d492af03f5b8
nrnr <callmev@outlook.com>
2025-03-14 23:22:57 +0800

    Update init.lua

--- vscode/init.lua
+++ vscode/init.lua
@@ -9,17 +9,22 @@ vim.o.hlsearch = true
 vim.o.cursorline = true
 vim.o.swapfile = false
 vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 vim.o.undodir = vim.fn.stdpath("data")
 -- 禁用Neovim的文件管理
 vim.g.loaded_newrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- 映射按键
 -- 大写 J 和 K 跳转5行
 vim.keymap.set('n', 'J', '5j')
 vim.keymap.set('n', 'K', '5k')
 -- 大写 H 和 L 行首和行尾
 vim.keymap.set('n', 'H', '^')
 vim.keymap.set('n', 'L', '$')
+-- 反向撤销
+vim.keymap.set('n', 'U', '<C-r>')
+-- 清除高亮
+vim.keymap.set('n', '<Esc>', ':nohlsearch<CR>')
 
 vim.keymap.set("n", "<leader>q", ":q<CR>", { silent = true })
