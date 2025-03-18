2b00c1a095e79d85aae0d188d5714e4678bba42c
nrnr <callmev@outlook.com>
2025-03-18 19:52:32 +0800

    Update Microsoft.PowerShell_profile.ps1

--- WINDOWS/PS/Microsoft.PowerShell_profile.ps1
+++ WINDOWS/PS/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,29 @@
-Import-Module posh-git # git
-Import-Module -Name Terminal-Icons  # 文件图标库
-Import-Module PSReadLine  # 提供语法高亮、错误提示、多行编辑、键绑定、历史记录搜索等功能
+Invoke-Expression (&starship init powershell)
+Import-Module -Name Terminal-Icons  # 引入文件图标库
+# 设置预测文本来源为历史记录
+# Set-PSReadLineOption -PredictionSource History 
+# 设置 Tab 键，命令行自动补全和提示
+Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete 
+# 用于临时更改当前 PowerShell 允许运行脚本而不受限制,不会影响其他会话或系统全局设置
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+# disable bell/beep sound
+Set-PSReadlineOption -BellStyle None
 
-Invoke-Expression (& { (lua $HOME/z.lua/z.lua --init powershell once enhanced) -join "`n" })
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
 
-# 设置主题
-oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH/robbyrussell.omp.json"  |  Invoke-Expression
+$env:HTTP_PROXY = "http://127.0.0.1:7890"
+$env:HTTPS_PROXY = "http://127.0.0.1:7890"
+$env:NO_PROXY = "localhost,127.0.0.1,::1,192.168.0.0/16"
 
- 
-# Set-PSReadLineOption -EditMode Emacs
-# Set-PSReadLineOption -PredictionSource HistoryAndPlugin
-# Set-PSReadLineOption -PredictionViewStyle ListView
-# Set-PSReadLineOption -BellStyle None
-# Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar 
+function cdnvim()
+{
+  Set-Location $env:LOCALAPPDATA/nvim
+}
 
-# Set-PSReadLineOption -PredictionSource History # 设置预测文本来源为历史记录
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete # 设置 Tab 键，命令行自动补全和提示
-# Set-PSReadLineKeyHandler -Key "Ctrl+d" -Function MenuComplete # 设置 Ctrl+d 为菜单补全和 Intellisense
-# Set-PSReadLineKeyHandler -Key UpArrow -Function HistorySearchBackward # 设置向上键为后向搜索历史记录
-# Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward # 设置向下键为前向搜索历史纪录
+function cdnvimdata()
+{
+  Set-Location $env:LOCALAPPDATA/nvim-data
+}
